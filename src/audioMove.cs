7f487718c66ad0cbccdb77d8cfa983df307f8383
Anton Solbjørg <4324290+Notnaton@users.noreply.github.com>
2022-03-02 17:34:48 +0100

    Removed if statements, used Vector3.min/max
    Removed if statements, used Vector3.min/max
    
    Set tracking to head bone

--- src/audioMove.cs
+++ src/audioMove.cs
@@ -1,4 +1,3 @@
-﻿
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,48 +14,19 @@ namespace Anton.audioMove
         public Vector3 lowLimit;
         public Vector3 highLimit;
 
-        public Vector3 clamp(Vector3 playerPosition, Vector3 lowLimit, Vector3 highLimit) 
+        public Vector3 clamp(Vector3 playerPos, Vector3 lowLimit, Vector3 highLimit) 
         {
-            if (playerPosition[0] > highLimit[0]) 
-            {
-                playerPosition[0] = highLimit[0];
-            }
-            if (playerPosition[1] > highLimit[1]) 
-            {
-                playerPosition[1] = highLimit[1];
-            }
-            if (playerPosition[2] > highLimit[2]) 
-            {
-                playerPosition[2] = highLimit[2];
-            }
-
-            if (playerPosition[0] < lowLimit[0]) 
-            {
-                playerPosition[0] = lowLimit[0];
-            }
-            if (playerPosition[1] < lowLimit[1]) 
-            {
-                playerPosition[1] = lowLimit[1];
-            }
-            if (playerPosition[2] < lowLimit[2]) 
-            {
-                playerPosition[2] = lowLimit[2];
-            }
-
-            return playerPosition;//new Vector3(-30,0,0);//
+            playerPos = Vector3.Min(playerPos, highLimit);
+            playerPos = Vector3.Max(playerPos, lowLimit);
+            return playerPos;
         }
 
         void Update()
         {
             playerPosition = Networking.LocalPlayer.GetPosition();
-            audioObject.transform.position = clamp(playerPosition, lowLimit, highLimit);//new Vector3(-30,0,0);//
-        }
-
-        void Start()
-        {
-            
+            playerPosition = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Head);
+            audioObject.transform.position = clamp(playerPosition, lowLimit, highLimit);
         }
     }
-    
 }
 
